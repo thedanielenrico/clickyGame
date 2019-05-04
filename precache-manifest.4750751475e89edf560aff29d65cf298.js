063d8b16760883654d7d65013086e633f5359bf5
thedanielenrico <thedanielenrico@gmail.com>
2019-05-04 13:04:47 -0700

    Updates

---
+++ precache-manifest.4750751475e89edf560aff29d65cf298.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd5fe69edce1daca80a79b5ae2f0d12",
+    "url": "//github.com/thedanielenrico/clickyGame/index.html"
+  },
+  {
+    "revision": "1bd75e0795a49a852c4b",
+    "url": "//github.com/thedanielenrico/clickyGame/static/css/main.8cc236f9.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "//github.com/thedanielenrico/clickyGame/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "1bd75e0795a49a852c4b",
+    "url": "//github.com/thedanielenrico/clickyGame/static/js/main.8922dace.chunk.js"
+  },
+  {
+    "revision": "3c1c6edbdce93c7c4185",
+    "url": "//github.com/thedanielenrico/clickyGame/static/js/runtime~main.014abdbf.js"
+  }
+]);
