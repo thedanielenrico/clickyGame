e6d21181dc9f4f0a0cdac0ddf02c6e26ec4dcad4
thedanielenrico <thedanielenrico@gmail.com>
2019-05-04 13:30:12 -0700

    heroku

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "../src/components/Header";
+import CardGrid from "./components/CardGrid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  state = {
+    score: 0,
+    highScore: 0,
+    message: "Click an image to begin! "
+  }
+
+  increaseScore = () => {
+
+    this.setState({
+      score: this.state.score + 1,
+      message: "You guessed correctly",
+      highScore: this.state.score + 1 > this.state.highScore ? this.state.score + 1 : this.state.highScore
+    })
+
+
+
+  }
+  resetScore = () => {
+    this.setState({score: 0, message: "You guessed incorrectly "})
+
+  }
+
+  render() {
+    console.log(this.state)
+    return <>
+      <Header score={this.state.score} highScore={this.state.highScore} message={this.state.message}/>
+      <CardGrid increaseScore={this.increaseScore} resetScore={this.resetScore}></CardGrid>
+
+    </>
+  }
+
 }
 
 export default App;
---
+++ src/ImageArray.js
@@ -0,0 +1,52 @@
+const imgArray = [
+    {
+        id: 1,
+        url: "/utils/images/1.jpg",
+    },
+    {
+        id: 2,
+        url: "/utils/images/2.jpg",
+    },
+    {
+        id: 3,
+        url: "/utils/images/3.jpg",
+    },
+    {
+        id: 4,
+        url: "/utils/images/4.jpg",
+    },
+    {
+        id: 5,
+        url: "/utils/images/5.jpg",
+    },
+    {
+        id: 6,
+        url: "/utils/images/6.jpg",
+    },
+    {
+        id: 7,
+        url: "/utils/images/7.jpg",
+    },
+    {
+        id: 8,
+        url: "/utils/images/8.jpg",
+    },
+    {
+        id: 9,
+        url: "/utils/images/9.jpg",
+    },
+    {
+        id: 10,
+        url: "/utils/images/10.jpg",
+    },
+    {
+        id: 11,
+        url: "/utils/images/11.jpg",
+    },
+    {
+        id: 12,
+        url: "/utils/images/12.jpg",
+    }
+
+];
+export default imgArray
